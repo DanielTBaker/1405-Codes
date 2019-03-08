a533f9c7a883b72d1463609d71eaa57b73cc8c39
Fang Xi Lin <flin@cita.utoronto.ca>
2019-03-08 17:10:49 -0500

    changed a hard-coded part

--- Shift.py
+++ Shift.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy.optimize import minimize
 
-def chi_check(X,z_f,z_var,pp_f,freqs):
+def chi_check(X,z_f,z_var,pp_f,freqs,ngates):
     dt, a = X
     num = (z_f - (a * pp_f * np.exp(-1j*2*np.pi*freqs*dt)))[1:] # consider k>0
-    chisq = np.sum(np.abs(num)**2) / (z_var * 2048/2)
+    chisq = np.sum(np.abs(num)**2) / (z_var * ngates/2)
     return chisq
 
-def template_match(template, z, offpulse_bins = None):
+def template_match(template, z, off_gates = None):
     """
     Template matching code from Pulsar Timing and Relativistic Gravity, Taylor 1992, appendix A
 
@@ -21,30 +21,30 @@ def template_match(template, z, offpulse_bins = None):
     z :
         Profile to match the template to
 
-    offpulse_bins :
-        indices of the off pulse in z. If not provided, assume bins below the median are off pulse.
+    off_gates :
+        indices of the off pulse in z. If not provided, assume gatess below the median are off pulse.
     """
-    nbins = template.size
+    ngates = template.size
 
-    t = np.linspace(0,1,nbins,endpoint=False) # 2048 is the number of bins per pulse
+    t = np.linspace(0,1,ngates,endpoint=False)
     t += (t[1]-t[0])/2
     ppdt = t[1]-t[0]
 
-    freqs = np.fft.rfftfreq(nbins,ppdt)
+    freqs = np.fft.rfftfreq(ngates,ppdt)
 
     template_f = np.fft.rfft(template)
     z_f = np.fft.rfft(z)
 
-    # if offpulse_bins is provided, use that to compute variance, otherwise, use bins below the median.
-    if offpulse_bins:
-        z_var = np.sum(np.var(z[offpulse_bins])) # variance in the off pulse, for chisquare purposes
+    # if off_gates is provided, use that to compute variance, otherwise, use gates below the median.
+    if off_gates:
+        z_var = np.sum(np.var(z[off_gates])) # variance in the off pulse, for chisquare purposes
     else:
         z_var = np.sum(np.var(z[z<np.median(z)]))
     xguess = [0.,1.]
-    dt, a = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs)).x
+    dt, a = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs,ngates)).x
     # error term is eq. A10 from the paper, massaged a bit.
-    dterr = np.sqrt( (z_var*nbins/2)/a / np.sum( ((2*np.pi*freqs)**2*(z_f*template_f.conj()*np.exp(1j*2*np.pi*freqs*dt)+z_f.conj()*template_f*np.exp(-1j*2*np.pi*freqs*dt)))[1:] ).real )
-    b = (z_f[0] - dt * template_f[0]).real/nbins
+    dterr = np.sqrt( (z_var*ngates/2)/a / np.sum( ((2*np.pi*freqs)**2*(z_f*template_f.conj()*np.exp(1j*2*np.pi*freqs*dt)+z_f.conj()*template_f*np.exp(-1j*2*np.pi*freqs*dt)))[1:] ).real )
+    b = (z_f[0] - dt * template_f[0]).real/ngates
     return dt, dterr, a, b
 
 def shift(z, dt):
@@ -62,6 +62,6 @@ def shift(z, dt):
         Phase to shift
     """
 
-    nbins = z.size
-    freqs = np.fft.rfftfreq(nbins,ppdt)
-    return np.fft.irfft(np.exp(-1j*2*np.pi*np.fft.rfftfreq(nbins,1./nbins)*dt)*np.fft.rfft(z))
+    ngates = z.size
+    freqs = np.fft.rfftfreq(ngates,ppdt)
+    return np.fft.irfft(np.exp(-1j*2*np.pi*np.fft.rfftfreq(ngates,1./ngates)*dt)*np.fft.rfft(z))
