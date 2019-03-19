b1a684289895e1976266e62329e3e4200a466e6d
Fang Xi Lin <flin@cita.utoronto.ca>
2019-03-19 12:02:23 -0400

    Fix bug in baseline value, added default minimization method, thanks Rob\!

--- Shift.py
+++ Shift.py
@@ -7,7 +7,7 @@ def chi_check(X,z_f,z_var,pp_f,freqs,ngates):
     chisq = np.sum(np.abs(num)**2) / (z_var * ngates/2)
     return chisq
 
-def template_match(template, z, off_gates = None):
+def template_match(template, z, off_gates = None, method='Nelder-Mead'):
     """
     Template matching code from Pulsar Timing and Relativistic Gravity, Taylor 1992, appendix A
 
@@ -23,6 +23,10 @@ def template_match(template, z, off_gates = None):
 
     off_gates :
         indices of the off pulse in z. If not provided, assume gatess below the median are off pulse.
+
+    method :
+        Method to use in scipy.optimize.minimize in the chisquare minimization. Nelder-Mead typically performs well, given a reasonable initial guess.
+        However, it does tend to converge to local minima if the inital guess is far from the true values.
     """
     ngates = template.size
 
@@ -41,10 +45,13 @@ def template_match(template, z, off_gates = None):
     else:
         z_var = np.sum(np.var(z[z<np.median(z)]))
     xguess = [0.,1.]
-    dt, a = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs,ngates)).x
+    minchisq = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs,ngates),method=method)
+    if minchisq.success != True:
+        print('Chi square minimization failed to converge. !!BEWARE!!')
+    dt, a = minchisq.x
     # error term is eq. A10 from the paper, massaged a bit.
     dterr = np.sqrt( (z_var*ngates/2)/a / np.sum( ((2*np.pi*freqs)**2*(z_f*template_f.conj()*np.exp(1j*2*np.pi*freqs*dt)+z_f.conj()*template_f*np.exp(-1j*2*np.pi*freqs*dt)))[1:] ).real )
-    b = (z_f[0] - dt * template_f[0]).real/ngates
+    b = (z_f[0] - a * template_f[0]).real/ngates
     return dt, dterr, a, b
 
 def shift(z, dt):
@@ -63,5 +70,5 @@ def shift(z, dt):
     """
 
     ngates = z.size
-    freqs = np.fft.rfftfreq(ngates,ppdt)
-    return np.fft.irfft(np.exp(-1j*2*np.pi*np.fft.rfftfreq(ngates,1./ngates)*dt)*np.fft.rfft(z))
+    freqs = np.fft.rfftfreq(ngates,1./ngates)
+    return np.fft.irfft(np.exp(-1j*2*np.pi*freqs*dt)*np.fft.rfft(z))
