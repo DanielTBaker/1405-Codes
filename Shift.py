51a903a003b8376c607f24a953d8d51c8c364718
Fang Xi Lin <flin@cita.utoronto.ca>
2019-03-20 12:22:18 -0400

    Added option for putting in your own initial guesses

--- Shift.py
+++ Shift.py
@@ -7,7 +7,7 @@ def chi_check(X,z_f,z_var,pp_f,freqs,ngates):
     chisq = np.sum(np.abs(num)**2) / (z_var * ngates/2)
     return chisq
 
-def template_match(template, z, off_gates = None, method='Nelder-Mead'):
+def template_match(template, z, off_gates = None, smart_guess = True, xguess=[0.,1.], method='Nelder-Mead'):
     """
     Template matching code from Pulsar Timing and Relativistic Gravity, Taylor 1992, appendix A
 
@@ -26,11 +26,20 @@ def template_match(template, z, off_gates = None, method='Nelder-Mead'):
     off_gates :
         indices of the off pulse in z. If not provided, assume gatess below the median are off pulse. Used to estimate errors.
 
+    smart_guess :
+        By default, we cross correlate template profile and profile to be matched, and use the highest bin as the initial guess for dt.
+
+    xguess :
+        Only used if smart_guess is set to False. Initial guess [dt, scaling] for the minimization.
+
     method :
         Method to use in scipy.optimize.minimize in the chisquare minimization. Nelder-Mead typically performs well, given a reasonable initial guess.
         However, it does tend to converge to local minima if the inital guess is far from the true values.
     """
     ngates = template.size
+
+    assert z.size == ngates, "Template profile and profile to fit are not of the same size. Perhaps rebin one of them?"
+    ## TODO: make it work despite the different lengths
 
     t = np.linspace(0,1,ngates,endpoint=False)
     t += (t[1]-t[0])/2
@@ -47,10 +56,11 @@ def template_match(template, z, off_gates = None, method='Nelder-Mead'):
     else:
         z_var = np.sum(np.var(z[z<np.median(z)]))
 
+    if smart_guess == True:
     # cross-correlate profiles, take peak bin to get starting guess
-    profcorr = np.fft.irfft( np.fft.rfft(z) * np.fft.rfft(template).conj() )
-    x = np.fft.fftfreq(len(template))
-    xguess = [x[np.argmax(profcorr)], 1.]
+        profcorr = np.fft.irfft( np.fft.rfft(z) * np.fft.rfft(template).conj() )
+        x = np.fft.fftfreq(len(template))
+        xguess = [x[np.argmax(profcorr)], 1.]
 
     minchisq = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs,ngates),method=method)
     if minchisq.success != True:
