84b39462326d3a2af51d4648adf24947a03f4053
Fang Xi Lin <flin@cita.utoronto.ca>
2019-03-21 22:07:39 -0400

    Explicit evaluation of scaling parameter
    The scaling parameter, a, can be found explicitly at every step since it's a linear parameter.

--- Shift.py
+++ Shift.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy.optimize import minimize
 
-def chi_check(X,z_f,z_var,pp_f,freqs,ngates):
-    dt, a = X
+def chi_check(dt,z_f,z_var,pp_f,freqs,ngates):
+    a = np.sum( (z_f*pp_f.conj()*np.exp(1j*2*np.pi*freqs*dt) + z_f.conj()*pp_f*np.exp(-1j*2*np.pi*freqs*dt))[1:] ) / np.sum( 2 * np.abs(pp_f[1:])**2 )
     num = (z_f - (a * pp_f * np.exp(-1j*2*np.pi*freqs*dt)))[1:] # consider k>0
     chisq = np.sum(np.abs(num)**2) / (z_var * ngates/2)
     return chisq
 
-def template_match(template, z, off_gates = None, smart_guess = True, xguess=[0.,1.], method='Nelder-Mead'):
+def template_match(template, z, off_gates = None, smart_guess = True, xguess=0., method='Nelder-Mead'):
     """
     Template matching code from Pulsar Timing and Relativistic Gravity, Taylor 1992, appendix A
 
@@ -30,7 +30,7 @@ def template_match(template, z, off_gates = None, smart_guess = True, xguess=[0.
         By default, we cross correlate template profile and profile to be matched, and use the highest bin as the initial guess for dt.
 
     xguess :
-        Only used if smart_guess is set to False. Initial guess [dt, scaling] for the minimization.
+        Only used if smart_guess is set to False. Initial guess `dt` for the minimization.
 
     method :
         Method to use in scipy.optimize.minimize in the chisquare minimization. Nelder-Mead typically performs well, given a reasonable initial guess.
@@ -60,15 +60,21 @@ def template_match(template, z, off_gates = None, smart_guess = True, xguess=[0.
     # cross-correlate profiles, take peak bin to get starting guess
         profcorr = np.fft.irfft( np.fft.rfft(z) * np.fft.rfft(template).conj() )
         x = np.fft.fftfreq(len(template))
-        xguess = [x[np.argmax(profcorr)], 1.]
+        xguess = x[np.argmax(profcorr)]
 
     minchisq = minimize(chi_check, x0=xguess, args=(z_f,z_var,template_f,freqs,ngates),method=method)
     if minchisq.success != True:
         print('Chi square minimization failed to converge. !!BEWARE!!')
-    dt, a = minchisq.x
+        
+    dt = minchisq.x
+    
     # error term is eq. A10 from the paper, massaged a bit.
     dterr = np.sqrt( (z_var*ngates/2)/a / np.sum( ((2*np.pi*freqs)**2*(z_f*template_f.conj()*np.exp(1j*2*np.pi*freqs*dt)+z_f.conj()*template_f*np.exp(-1j*2*np.pi*freqs*dt)))[1:] ).real )
+    
+    a = np.sum( (z_f*template_f.conj()*np.exp(1j*2*np.pi*freqs*dt) + z_f.conj()*template_f*np.exp(-1j*2*np.pi*freqs*dt))[1:] ) / np.sum( 2 * np.abs(template_f[1:])**2 )
+    
     b = (z_f[0] - a * template_f[0]).real/ngates
+    
     return dt, dterr, a, b
 
 def shift(z, dt):
